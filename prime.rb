ea56029c188513c8861f38b54e8d09f9e62c6c2e
ayo3able <mrsbarclay101008@gmail.com>
2020-10-13 01:17:44 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,22 +1,21 @@
-require 'prime'
-def sorting(top)
-    prime_numbers = (0..top).to_a
-  
-    prime_numbers[0] = prime_numbers[1] = nil
-  
-    counter = 0
-    prime_numbers.each do |i|
-      next unless i
-      break if i*i > top
-      counter += 1
-      
-      (i*i).step(top,i) { |y| prime_numbers[y] = nil }
-    end
-    prime_numbers.compact
-  end
 
-  def prime?(number)
-    sorting(number).include?(number)
-  end
-  
-
+   require 'benchmark'
+   require 'bigdecimal/math'
+   
+   puts Benchmark.measure { BigMath.PI(10_000) }
+   def sieve(max)
+     primes = (0..max).to_a
+     primes[0] = primes[1] = nil
+     counter = 0
+     primes.each do |p|
+       next unless p   
+       break if p*p > max
+       counter += 1
+       (p*p).step(max,p) { |m| primes[m] = nil }
+     end
+     primes.compact
+   end
+   
+   def prime?(num)
+     sieve(num).include?(num)
+   end
